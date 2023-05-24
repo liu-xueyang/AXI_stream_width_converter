2c501442c62a0e434e95608affc7c5d426e726fe
Xueyang Liu <marliu@umich.edu>
2023-05-24 13:38:22 -0700

    Downsizer first try

--- Downsizer.sv
+++ Downsizer.sv
@@ -1,31 +1,32 @@
-module axis_dwidth_upsizer #(
-  parameter WIDTH      = 32, // input width
+module axis_dwidth_downsizer #(
+  parameter WIDTH      = 32, // output width
   parameter NUM_REG    =  2 // Scale factor
 )(
     input  logic aclk,                    // input wire aclk
     input  logic aresetn,              // input wire aresetn
     input  logic s_axis_tvalid,  // input wire s_axis_tvalid
     output logic s_axis_tready,  // output wire s_axis_tready
-    input  logic [WIDTH-1:0] s_axis_tdata,    // input wire [31 : 0] s_axis_tdata
+    input  logic [WIDTH*NUM_REG-1:0] s_axis_tdata,    // input wire [63 : 0] s_axis_tdata
     input  logic s_axis_tlast,   
 
     output logic m_axis_tvalid,  // output wire m_axis_tvalid
     input  logic m_axis_tready,  // input wire m_axis_tready
-    output logic [WIDTH*NUM_REG-1:0] m_axis_tdata,    // output wire [63 : 0] m_axis_tdata
+    output logic [WIDTH-1:0] m_axis_tdata,    // output wire [31 : 0] m_axis_tdata
     output logic m_axis_tlast
 );
   // Define the states as enum
     localparam READY = 0;
-    localparam LOAD = 1;
-    localparam WRTARRAY = 2;
+    localparam SHIFT = 1;
+    localparam LAST = 2;
 
   logic [NUM_REG*WIDTH - 1:0] s_reg;
   logic [32:0] cnt;
   // Define state register and next state variables
   logic [1:0] state_reg;
 //   logic [1:0] next_state;
+    
   always @(posedge aclk) begin
-    m_axis_tdata    <= s_reg;
+    m_axis_tdata <= s_reg[WIDTH - 1:0];
     if (aresetn == 0) begin
       state_reg       <= READY;
       cnt             <=  0;
@@ -33,40 +34,40 @@ module axis_dwidth_upsizer #(
       m_axis_tvalid   <=  0;
       s_reg           <= '0;
     end else begin
+    //   m_axis_tdata    <= s_reg;
       // State transitions and outputs
       case (state_reg)
         READY:
           begin
             cnt <=  0;
             if (s_axis_tvalid == 1) begin
-              s_reg[WIDTH-1:0] <= s_axis_tdata;
-              state_reg   <= LOAD;
-              cnt           <= cnt + 1;	
+              s_reg       <= s_axis_tdata;
+              state_reg   <= SHIFT;
+              s_axis_tready <= 0;
+              m_axis_tvalid <= 1;
             end
           end
-        LOAD:
+        SHIFT:
           begin
-            if (cnt < NUM_REG  & s_axis_tvalid == 1) begin
-              s_reg <= (s_reg << WIDTH) | s_axis_tdata;
-              cnt            <= cnt + 1;
-              state_reg      <= LOAD;
-            end else if (cnt < NUM_REG & s_axis_tvalid == 0) 
-              state_reg <= LOAD;
+            if (cnt < NUM_REG - 1  & m_axis_tready == 1) begin
+                cnt   <= cnt + 1;
+                s_reg <= s_reg >> WIDTH;
+            end else if (cnt < NUM_REG - 1 & m_axis_tready == 0) 
+                state_reg <= SHIFT;
             else begin
-              state_reg       <= WRTARRAY;
-              s_axis_tready   <= 0;
-              m_axis_tvalid   <= 1;
-              cnt <= 0;
-            end
+                state_reg <= LAST;
+	            m_axis_tvalid <= 0;
+	        end
           end
-        WRTARRAY:
+        LAST:
           begin
             if (m_axis_tready == 1) begin
-              state_reg       <= READY;
-              m_axis_tvalid <= 0;
-              s_axis_tready   <= 1;
-            end
+                state_reg     <= READY;
+                s_axis_tready <= 1;	
+	        end
           end
+        
+        // Define transitions for other states
       endcase
     end
   end
--- Downsizer_tb.sv
+++ Downsizer_tb.sv
@@ -15,32 +15,32 @@ module test;
   always #5 clk = !clk;
 
   reg s_axis_tvalid = 0;
-  reg [WIDTH-1:0] s_axis_tdata = 100;
+  reg [WIDTH*NUM_REG-1:0] s_axis_tdata = (1 << WIDTH) + 100;
   reg s_axis_tlast = 0;
   reg m_axis_tready = 1;
   initial begin
     #100 s_axis_tvalid = 1;
   end
-  always #10 s_axis_tdata = s_axis_tdata + 16;
+  always #20 s_axis_tdata = s_axis_tdata + (1 << WIDTH) + 16;
 
   wire s_axis_tready;
   wire m_axis_tvalid;
-  wire [WIDTH*NUM_REG-1:0] m_axis_tdata;
+  wire [WIDTH-1:0] m_axis_tdata;
   wire m_axis_tlast;
-  axis_dwidth_upsizer upsizer (.aclk(clk),                    // input wire aclk
+  axis_dwidth_downsizer downsizer (.aclk(clk),                    // input wire aclk
     .aresetn(reset),              // input wire aresetn
     .s_axis_tvalid(s_axis_tvalid),  // input wire s_axis_tvalid
     .s_axis_tready(s_axis_tready),  // output wire s_axis_tready
-    .s_axis_tdata(s_axis_tdata),    // input wire [31 : 0] s_axis_tdata
+    .s_axis_tdata(s_axis_tdata),    // input wire [64 : 0] s_axis_tdata
     .s_axis_tlast(s_axis_tlast),   
 
     .m_axis_tvalid(m_axis_tvalid),  // output wire m_axis_tvalid
     .m_axis_tready(m_axis_tready),  // input wire m_axis_tready
-    .m_axis_tdata(m_axis_tdata),    // output wire [63 : 0] m_axis_tdata
+    .m_axis_tdata(m_axis_tdata),    // output wire [32 : 0] m_axis_tdata
     .m_axis_tlast(m_axis_tlast));
 
   initial begin
-    $dumpfile("test_upsizer.vcd");
+    $dumpfile("test_downsizer.vcd");
     $dumpvars(0,test);
     
     // $monitor("At time %t, value = %h (%0d)",
